df49dff938ea43a9fee5d0b901b4d10f6c488066
John Lee <johnnyweasled@gmail.com>
2023-01-25 13:21:37 -0500

    Fixed forward euler time steps

--- 306_hw2.py
+++ 306_hw2.py
@@ -1,7 +1,8 @@
-from sklearn.linear_model import LinearRegression as LR
+#from sklearn.linear_model import LinearRegression as LR
 import numpy as np
-import math
 import matplotlib.pyplot as plt
+
+def analytic()
 
 t0 = 0
 tEnd = 100  # hours
@@ -13,8 +14,11 @@ n = int((tEnd - t0)/dt)
 
 m = np.zeros(n)
 p1 = np.zeros(n)
-p2 = np.zeros(n)
-p3 = np.zeros(n)
+p2 = np.zeros(int((tEnd - t0)/5))
+p3 = np.zeros(int((tEnd - t0)/10))
+
+print(len(t), len(t5), len(t10))
+print(len(p1), len(p2), len(p3))
 
 m[0]  = 0
 p1[0] = 0
@@ -41,15 +45,13 @@ for i in range (0, len(t)-1):
     m[i+1] = m[i] + dt * (kr - m[i]*(yr + u))
     p1[i+1] = p1[i] + dt * (kp * m[i] - p1[i] * (yp + u))
 
-    if i % 50 == 0:
-        p2[i+1] = p2[i] + 5 * (kp * m[i] - p2[i] * (yp + u))
+for j in range(0, len(t5)-1):
+    p2[i+1] = p2[i] + 5 * (kp * m[i] - p2[i] * (yp + u))
     
-    else
-    
+for k in range(0, len(t10)-1):
+    p3[i+1] = p3[i] + 10 * (kp * m[i] - p3[i] * (yp + u))
 
-    
-    if i % 10 == 0:
-        p3[i+1] = p3[i] + 10 * (kp * m[i] - p3[i] * (yp + u))
+
 
 
 #x = [item*500*np.exp(2.3) for item in t]
@@ -57,12 +59,12 @@ x = [item for item in t]
 
 fig = plt.figure(num=1, clear=True)
 ax = fig.add_subplot(1,1,1)
-ax.plot(t, protein_analytic, '-b', label = "Analytic mRNA")
-#ax.plot(t, protein_analytic_2, '-r', label = "mRNA_ss")
+#ax.plot(t, protein_analytic, '-b', label = "Analytic mRNA")
+ax.plot(t, protein_analytic_2, '-r', label = "mRNA_ss")
 # ax.plot(t, p1, '--g')
 ax.plot(t, p1, '-g', label = "dt = 0.1")
-ax.plot(t, p2, label = "dt = 5")
-ax.plot(t, p3, label = "dt = 10")
+ax.plot(t5, p2, label = "dt = 5")
+ax.plot(t10, p3, label = "dt = 10")
 ax.set(xlabel = 'time (hours)', ylabel = 'Proteins (molecule count)', title = 'Comparison of Analytical and Numerical')
 ax.grid(True), fig.tight_layout(), ax.legend()
 
@@ -73,6 +75,3 @@ ax.grid(True), fig.tight_layout(), ax.legend()
 # ax.set(xlabel = 'time (hours)', ylabel = 'Proteins (molecule count)', title = 'Comparison of Analytical and Numerical')
 # ax.grid(True), fig.tight_layout()
 plt.show()
-
-print(n)
-print(len(t5))
