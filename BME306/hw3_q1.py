73f7f92488d54aa8cec3cce917a0a968896409d7
John Lee <johnnyweasled@gmail.com>
2023-02-01 21:02:06 -0500

    Readable comments
    added comments to code for Q1.B

--- BME306/hw3_q1.py
+++ BME306/hw3_q1.py
@@ -1,35 +1,42 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# defining given constants
 ker = 0.0284    # min^-1
 kg = 0.0303     # min^-1
 kpm = 0.00255   # min^-1
 mu = 20
 
+# defining time bounds and empty arrays
 t0 = 0
 tEnd = 600  # min
 dt = 0.1
-
 t = np.arange(t0, tEnd, 0.1)
 n = int((tEnd - t0)/dt)
 m = np.zeros(n)
 per = np.zeros(n)
 pg = np.zeros(n)
 ppm = np.zeros(n)
+total= np.zeros(n)
 
+# initializing arrays at t=0
 per[0] = 20
-pg[0], ppm[0] = 0, 0
+pg[0], ppm[0], total[0] = 0, 0, 20
 
+# forward euler with differential equations
 for i in range(0, len(t)-1):
     per[i+1] = per[i] + dt * (-ker * per[i])
     pg[i+1] = pg[i] + dt * (-kg * pg[i] + ker*per[i])
     ppm[i+1] = ppm[i] + dt * (-kpm * ppm[i] + kg*pg[i])
+    total [i+1] = per[i+1] + pg[i+1] + ppm[i+1]
 
+# plotting 
 fig = plt.figure(num = 2, clear = True)
 ax = fig.add_subplot(1, 1, 1)
-ax.plot(t, per, '-b', label = 'Analyltic mRNA')
-ax.plot(t, pg, '-r', label = 'mRNA_ss constants')
-ax.plot(t, ppm, '--g', label = 'dt = 0.1')
-ax.set(xlabel = 'time (hours)', ylabel = 'Proteins (molecule/uM^3)', title = 'Comparison of Analytical and Numerical')
+ax.plot(t, per, '-b', label = 'ER')
+ax.plot(t, pg, '-r', label = 'PM')
+ax.plot(t, ppm, '-g', label = 'Golgi')
+ax.plot(t, total, label = 'Total')
+ax.set(xlabel = 'time (minutes)', ylabel = 'VSVG-GfP (x10^6)', title = 'Cargo Protein (VSVG-GFP) in Compartments')
 ax.grid(True), fig.tight_layout(), ax.legend()
 plt.show()
